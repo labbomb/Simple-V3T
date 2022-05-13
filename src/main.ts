f5d78ed401fd509d4c0ef4b5492c1bfd54873665
labbomb <739955946@qq.com>
2022-05-13 15:54:53 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './assets/styles/default.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 app.use(router)
+app.use(pinia)
 app.mount('#app')
