d7a222f830a8d47828ad3f69c8f900ff8ed7e7ff
labbomb <739955946@qq.com>
2022-05-14 11:49:28 +0800

    Delete useless information

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 import { DefineComponent } from 'vue'
 
 declare module '*.vue' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
